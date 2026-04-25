43f85be62c046ee0ccb419eabb71c785c3a301c9
jasleen <159606015+jasleen2005@users.noreply.github.com>
2026-04-25 20:58:49 +0530

    Update settings.py

--- RnD_CRIS_Frm/VendorFormProject/VendorFormProject/settings.py
+++ RnD_CRIS_Frm/VendorFormProject/VendorFormProject/settings.py
@@ -264,27 +264,27 @@ TEMPLATES = [
 # --------------------
 # DATABASE (PostgreSQL)
 # --------------------
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'vendor_database_final',
-#         'USER': 'postgres',
-#         'PASSWORD': 'root',  # Change if needed
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("POSTGRES_DB"),
-        'USER': os.environ.get("POSTGRES_USER"),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
-        'HOST': os.environ.get("POSTGRES_HOST"),
-        'PORT': os.environ.get("POSTGRES_PORT"),
+        'NAME': 'vendor_database_final',
+        'USER': 'postgres',
+        'PASSWORD': 'root',  # Change if needed
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get("POSTGRES_DB"),
+#         'USER': os.environ.get("POSTGRES_USER"),
+#         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+#         'HOST': os.environ.get("POSTGRES_HOST"),
+#         'PORT': os.environ.get("POSTGRES_PORT"),
+#     }
+# }
 
 # --------------------
 # PASSWORD VALIDATION
